12e1b5937c28598a5f4970abeaae9a7fada090bb
mattiabenelli <benellimattia0@gmail.com>
2023-02-15 14:26:33 +0100

    es done

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    
+    $data = [
+        'saluto' => 'Hello vue' 
+    ];
+    return view('index', $data);
 });
